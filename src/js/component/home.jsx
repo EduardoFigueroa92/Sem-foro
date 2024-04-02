0e8045341e504100bb52441579ea5f4d196e1a63
Eduardo Figueroa <156800901+EduardoFigueroa92@users.noreply.github.com>
2024-04-02 14:59:39 +0000

    First commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,22 +7,24 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	const [ selectedColor, setSelectedColor ] = useState("");
 	return (
-		<div className="text-center mt-5">
-			<div className="contenedorSemaforo">
+		<div className = "fondo">
+			<div className="text-center mt-5">
+				<div className="contenedorSemaforo">
 
-				<div onClick= {() => setSelectedColor("red")} 
-				className={"lightRed" + (selectedColor === "red" ? " glow" : "")}>
+					<div onClick= {() => setSelectedColor("red")} 
+					className={"lightRed" + (selectedColor === "red" ? " glow" : "")}>
 
-				</div>
-				<div onClick= {() => setSelectedColor("yellow")} 
-				className={"lightYellow" + (selectedColor === "yellow" ? " glow" : "")}>
+					</div>
+					<div onClick= {() => setSelectedColor("yellow")} 
+					className={"lightYellow" + (selectedColor === "yellow" ? " glow" : "")}>
 
-				</div>
-				<div  onClick= {() => setSelectedColor("green")} 
-				className={"lightGreen"  + (selectedColor === "green" ? " glow" : "")}>
+					</div>
+					<div  onClick= {() => setSelectedColor("green")} 
+					className={"lightGreen"  + (selectedColor === "green" ? " glow" : "")}>
 
-				</div>
+					</div>
 				
+				</div>
 			</div>
 		</div>
 	);
